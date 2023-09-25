25d4aa72765f48cc227420ae3d66209941597c39
purajit <malalur2@illinois.edu>
2023-09-25 16:33:08 -0700

    Configure doom emacs the way we like

--- emacs/_doom.d/packages.el
+++ emacs/_doom.d/packages.el
@@ -7,10 +7,7 @@
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;; (package! indent-guide-mode)
-(package! indent-guide)
-(package! terraform-mode)
-(package! yaml-mode)
+;; (package! terraform-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
