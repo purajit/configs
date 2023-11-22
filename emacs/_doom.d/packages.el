a5ef34c848becea103cb7d201ae52e3cccaed4d2
purajit <malalur2@illinois.edu>
2023-11-21 19:09:14 -0500

    add ruff, other small changes (#29)

--- emacs/_doom.d/packages.el
+++ emacs/_doom.d/packages.el
@@ -9,6 +9,8 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;; (package! terraform-mode)
 
+(package! ruff-format)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
