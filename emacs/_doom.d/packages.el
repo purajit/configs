cbbc74cf585bd91547178414b11faae8495d547a
purajit <malalur2@illinois.edu>
2024-08-25 04:06:05 -0700

    updates (#39)

--- emacs/_doom.d/packages.el
+++ emacs/_doom.d/packages.el
@@ -10,6 +10,8 @@
 ;; (package! terraform-mode)
 
 (package! ruff-format)
+(package! flycheck-package)
+(package! consult-flycheck)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
