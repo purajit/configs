5453e37d97769dd91dc95f631de8dbf2a105e8da
purajit <malalur2@illinois.edu>
2024-09-13 03:27:13 -0700

    better emacs config (#78)

--- doom/config.el
+++ doom/config.el
@@ -1,4 +1,8 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;;; See https://github.com/doomemacs/doomemacs/blob/master/templates/config.example.el
+
+(setq user-full-name "purajit")
+;; (setq user-mail-address "")
 
 ;;;;;; KEYS
 ;; option-as-meta, command-as-hyper
@@ -11,19 +15,18 @@
 (global-set-key (kbd "M-g") 'goto-line)
 (global-set-key (kbd "C-c SPC") 'avy-goto-char-timer)
 (global-set-key (kbd "C-c C-g") 'magit-status)
+;; to get replacement info in modeline
+(global-set-key [remap query-replace] 'anzu-query-replace)
+(global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
 (global-unset-key (kbd "<magnify-up>"))
 (global-unset-key (kbd "<magnify-down>"))
-
-(setq org-directory "~/Documents/org/")
-
 ;; use consult's buffer explorer and override default
 (global-set-key (kbd "C-x b") 'consult-buffer)
-(vertico-reverse-mode)
-(setq display-line-numbers-type t)
 (setq tab-always-indent t)
-(setq confirm-kill-emacs nil)
+
 
 ;;;;;; CODING MODES
+(setq doom-scratch-initial-major-mode 'text-mode)
 (add-hook 'python-mode-hook 'ruff-format-on-save-mode)
 (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode)
 (setq js-indent-level 2)
@@ -36,19 +39,38 @@
 (setq highlight-indent-guides-responsive 'top)
 (setq highlight-indent-guides-auto-character-face-perc '75)
 (setq highlight-indent-guides-auto-top-character-face-perc '300)
-
 (after! git-commit
   ;; 72 is the GitHub limit
   (setq git-commit-summary-max-length 72))
+(after! lsp-mode
+  (setq lsp-enable-symbol-highlighting nil
+        ;; If an LSP server isn't present when I start a prog-mode buffer, you
+        ;; don't need to tell me. I know. On some machines I don't care to have
+        ;; a whole development environment for some ecosystems.
+        lsp-enable-suggest-server-download nil))
 
+
+;;;;;; APPEARANCE
+(vertico-reverse-mode)
+;; font, themes, coordinate background with terminal
 (setq doom-font (font-spec :family "Mononoki Nerd Font" :size 14 :weight 'regular))
-(setq doom-theme 'doom-molokai)
-;; same as Alacritty
+(setq doom-theme 'doom-tomorrow-night)
 (custom-set-faces
   '(default ((t (:background "#181818")))))
 (custom-set-faces
   '(solaire-default-face ((t (:background "#181818")))))
-
+;; bring GUI window opened from terminal to focus
+(select-frame-set-input-focus (selected-frame))
+(setq default-frame-alist '((left . 0) (width . 120) (fullscreen . fullheight)))
+;; Prevents some cases of Emacs flickering.
+(add-to-list 'default-frame-alist '(inhibit-double-buffering . t))
+(menu-bar-mode -1)
+;; line numbers are a _massive_ hit to performance
+(setq display-line-numbers-type nil)
+;; to get search result counts in modeline
+(global-anzu-mode +1)
+(anzu-mode +1)
+;; the long-awaited modeline!
 (after! doom-modeline
   (setq doom-modeline-height 25)
   (setq doom-modeline-bar-width 3)
@@ -71,77 +93,7 @@
     "Display the vcs name."
     (and vc-mode (cadr (split-string (string-trim vc-mode) "^[A-Z]+[-:]+")))))
 
-(select-frame-set-input-focus (selected-frame))
-(setq menu-bar-mode nil)
-(global-anzu-mode +1)
-(anzu-mode +1)
-(global-set-key [remap query-replace] 'anzu-query-replace)
-(global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;; (setq doom-theme 'doom-one)
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
+;;;;;; MISC
+(setq org-directory "~/Documents/org/")
+(setq confirm-kill-emacs nil)
