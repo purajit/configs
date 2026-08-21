67c7e73c539c9d9671cfb40cd34475f828d4d493
purajit <7026198+purajit@users.noreply.github.com>
2026-08-21 01:47:10 -0700

    keyboard shortcuts

--- setup.sh
+++ setup.sh
@@ -203,6 +203,10 @@ function setup_defaults {
     '{ HIDKeyboardModifierMappingSrc = 30064771129; HIDKeyboardModifierMappingDst = 30064771300; }'
   printf "%s%s Mapped Caps Lock to Right Control\n" "${GREEN}" "${RESET}"
 
+  defaults import com.apple.symbolichotkeys "${CONFIG_HOME}/macos/symbolichotkeys.plist"
+  printf "%s%s Disabled 63 unwanted macOS symbolic keyboard shortcuts\n" "${GREEN}" "${RESET}"
+  printf "%s%s Command-Shift-3/4 will copy screenshots; add Control to save them to files\n" "${GREEN}" "${RESET}"
+
   defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 3
   for trackpad_domain in \
     com.apple.AppleMultitouchTrackpad \
